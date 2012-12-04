d3e1650e3a3b82267bb1eb11b23c0f4d6ae0b1a3
Mike Crute <mcrute@gmail.com>
2012-12-04 16:52:26 -0500

    Enable admin site

--- codemash/urls.py
+++ codemash/urls.py
@@ -1,19 +1,18 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
     (r'^contact/', include('contact.urls', namespace='contact')),
     (r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
     (r'^accounts/', include('accounts.urls', namespace='account')),
 
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
     url(r'^$', 'pages.views.home', name='home'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
