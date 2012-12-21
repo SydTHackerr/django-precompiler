6ba89b2cb334b0ef43519eeb02d8d6ac0cd20264
Mike Crute <mcrute@gmail.com>
2012-12-20 21:56:13 -0500

    Add flatpages for CMS

--- codemash/settings.py
+++ codemash/settings.py
@@ -93,6 +93,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -124,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django.contrib.flatpages',
 
     'contact',
     'accounts',
--- codemash/urls.py
+++ codemash/urls.py
@@ -13,6 +13,6 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', 'pages.views.home', name='home'),
-
+    url(r'^$', 'django.contrib.flatpages.views.flatpage', { 'url': '/' },
+        name='home'),
 )
