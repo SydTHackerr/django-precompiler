016d3ffa6d430d7ddeafc4125bf4d5121af05ace
Mike Crute <mcrute@gmail.com>
2013-01-07 18:56:02 -0500

    Ex 7: Django Admin

--- codesmash/urls.py
+++ codesmash/urls.py
@@ -1,18 +1,13 @@
+from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^contact/', include('contact.urls', namespace='contact')),
     url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^accounts/', include('accounts.urls', namespace='account')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'codesmash.views.home', name='home'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
