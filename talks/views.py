3841c78484d4eb259314deb7ba678b4f03e299d2
Mike Crute <mcrute@gmail.com>
2013-01-08 00:11:49 -0500

    Ex 12: Querying the Model

--- talks/views.py
+++ talks/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+
+from talks.models import Talk
+
+
+def all_talks(request):
+    return render(request, "talks/index.html", {
+        "object_list": Talk.objects.all(),
+    })
